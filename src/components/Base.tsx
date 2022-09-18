64245540d1ebfed84d1f9930a3f388b1bf1bbb8b
Lorenzo Cueto <lorenzo.cueto@signifyd.com>
2022-09-18 21:45:08 +0100

    messing around with react router

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,40 +1,21 @@
-import React, { useState, useEffect, useCallback } from "react";
-
-import CurrenciesHolder from "./CurrenciesHolder";
-import SubClassHolder from "./SubClassHolder";
-import ArmorHolder from "./ArmorHolder";
-import WeaponHolder from "./WeaponHolder";
-
-export const baseContext = React.createContext(0);
+import { Routes, Route } from "react-router-dom";
+import CharacterScreen from "./CharacterScreen";
+import {
+  Comp404,
+  RandomPage,
+  PageSelect,
+  CharacterVault,
+  CharacterPage,
+} from "pagescomponents/pageIndex";
 
 const Base = () => {
-  const [armorLight, setArmorLight] = useState(0);
-  const [weaponLight, setWeaponLight] = useState(0);
-
-  const getArmorLight = (newLight:number) => {
-    setArmorLight(newLight);
-  };
-
-  const getWeaponLight = (newLight:number) => {
-    setWeaponLight(newLight);
-  };
   return (
-    <div className="Page">
-      <div className="topBanner"></div>
-
-      <div className="characterScreen">
-        <WeaponHolder lightSetter = {setWeaponLight}/>
-        <div className="backgroundCharacter"></div>
-
-        <div className="statsBar">
-          <CurrenciesHolder></CurrenciesHolder>
-          <div className="lightContainer">
-            <h1>Light Level</h1>
-            <h1>{Math.floor(armorLight/9)}</h1>
-          </div>
-          <ArmorHolder lightSetter={setArmorLight} />
-        </div>
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<PageSelect />} />
+        <Route path="/character" element={<CharacterPage />}></Route>
+        <Route path="*" element={<Comp404 />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pagescomponents/RandomPage.tsx
@@ -0,0 +1,9 @@
+const RandomPage = () => {
+  return (
+    <div>
+      <h1>Another Page</h1>
+    </div>
+  );
+};
+
+export default RandomPage;
